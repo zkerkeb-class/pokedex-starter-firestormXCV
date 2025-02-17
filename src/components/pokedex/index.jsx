549af676f63eabc3357a40ca17bc89988725f53f
tpasquier <thomas.pasquier@dataxion-france.fr>
2025-02-17 14:06:40 +0100

    delete unused import

--- src/components/pokedex/index.jsx
+++ src/components/pokedex/index.jsx
@@ -1,7 +1,6 @@
 
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import pokemons from '../../assets/pokemons'
-import PokedexRow from '../../components/pokedexRow'
 import PokemonCard from "../pokemonCard";
 import "./index.css"
 
